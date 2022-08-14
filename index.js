7408f445493b1e1031b68838c8c18f40051b5a4b
Thomas Broadley <buriedunderbooks@hotmail.com>
2022-08-14 09:37:57 -0400

    Add a single soul

--- index.js
+++ index.js
@@ -11,11 +11,24 @@
 let deathCursor;
 let lastMousePos;
 
+let souls = [];
+let score = 0;
+
+class SoulObject extends EngineObject {
+  constructor(startingPos, direction) {
+    super(startingPos, vec2(2, 2), 16, vec2(16, 16));
+    this.velocity = vec2(0, 0.1).rotate(90 * direction);
+    this.damping = 1;
+  }
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 function gameInit() {
   canvasFixedSize = vec2(640, 480);
 
   deathCursor = new EngineObject(mousePos, vec2(4, 4), 0, vec2(32, 32));
+
+  souls.push(new SoulObject(vec2(0, 0), 0));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -25,7 +38,7 @@ function gameUpdate() {
   if (lastMousePos) {
     const mouseMovement = mousePos.subtract(lastMousePos);
 
-    if (mouseMovement.length() < 0.1) {
+    if (mouseMovement.length() === 0) {
       deathCursor.tileIndex = 2;
     } else {
       deathCursor.tileIndex = mouseMovement.direction();
@@ -33,6 +46,8 @@ function gameUpdate() {
   }
 
   lastMousePos = mousePos;
+
+  console.log(engineObjects);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
